e6d55c767be28ff26c22fd8f51268fa1511eb9f8
Vea Novenario <veanovenario@gmail.com>
2025-11-27 18:17:37 +0800

    Added filtering of featured works

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,12 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const yearsExp = 16;
-  const projectsCount = 40;
+  const yearsExp = "16";
+  const overallProjectCount = "45+";
+  const solutionsArchitectProjectCount = "~15";
+  const technicalLeadProjectCount = "~18";
+  const developerAppCount = "~47";
 
   document.querySelectorAll(".years-exp").forEach(el => {
     el.textContent = yearsExp;
   });
 
-  document.querySelectorAll(".projects-count").forEach(el => {
-    el.textContent = projectsCount;
+  document.querySelectorAll(".projects-overall-count").forEach(el => {
+    el.textContent = overallProjectCount;
+  });
+
+  document.querySelectorAll(".projects-sa-count").forEach(el => {
+    el.textContent = solutionsArchitectProjectCount;
+  });
+
+  document.querySelectorAll(".projects-tl-count").forEach(el => {
+    el.textContent = technicalLeadProjectCount;
+  });
+
+  document.querySelectorAll(".projects-dev-count").forEach(el => {
+    el.textContent = developerAppCount;
   });
 });
